42b162223b47085e9b9d467531a80eb1a35a8984
Mord4x <elieaouad1997@gmail.com>
2021-03-22 16:05:16 +0200

    finalyearproject

---
+++ lib/Quiz1_main.dart
@@ -0,0 +1,33 @@
+import 'Quiz1q&a.dart';
+
+class Quiz1Main {
+  int _questionNumber = 0;
+  List<Question> _questions = [
+    Question('Dart is an Object oriented language.', true),
+    Question(
+        'Dart is developed by Google and is used to build mobile apps only.',
+        false),
+    Question('Dart is a dynamically typed.', false),
+    Question('Dart has a dynamic variable option.', true),
+    Question('Dart like typescript supports JSON files.', true),
+    Question('Dart apps work on Android only.', false),
+    Question('Browsers can execute Dart code directly.', false),
+    Question('Dart is fun.', false),
+    Question('Dart is used to make building large scale apps easier.', true),
+    Question('Dart and JavaScript are the same.', true),
+  ];
+
+  void nextQuestion() {
+    if (_questionNumber < _questions.length - 1) {
+      _questionNumber++;
+    }
+  }
+
+  String getQuestionText() {
+    return _questions[_questionNumber].questionText;
+  }
+
+  bool getQuestionAnswer() {
+    return _questions[_questionNumber].questionAnswer;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:final_year_project/Quiz1.dart';
+import 'package:final_year_project/Quiz2.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text("Quiz Page"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Center(
+              child: Text('Complete one of the following Quiz Types',
+                  style: TextStyle(fontSize: 18)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                    width: 200,
+                    height: 150,
+                    /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                    child: Image.asset('asset/images/download.png')),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => Quizzler()));
+                },
+                child: Text(
+                  'Dart True or False Quiz',
+                  style: TextStyle(color: Colors.blueAccent, fontSize: 25),
+                ),
+              ),
+            ),
+            Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => Quiz2()));
+                },
+                child: Text(
+                  'MCQ Quiz',
+                  style: TextStyle(color: Colors.blueAccent, fontSize: 25),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
